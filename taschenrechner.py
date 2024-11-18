ff976e7efb6c7871cda381513c51a64325699788
AnneBlaschke <anne.blaschke@deutschebahn.com>
2024-11-18 14:53:04 +0100

    Upload erster Trainingstag

---
+++ taschenrechner.py
@@ -0,0 +1,10 @@
+number1 = input('Bitte erste Zahl eingeben: ')
+number2 = input('Bitte zweite Zahl eingeben: ')
+
+number1 = float(number1)
+number2 = float(number2)
+
+addition = number1 + number2
+
+print('Die Summe der beiben Zahlen ist ' +str(addition))
+print('Die Differenz der beiden Zahlen ')
